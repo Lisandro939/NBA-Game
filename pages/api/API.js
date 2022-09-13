5ffda493a4432da04124ec0fb1c95d3e1626bfa7
Lisandro939 <lisandrofernandez2705@gmail.com>
2022-09-13 16:12:41 -0300

    first commit

---
+++ pages/api/API.js
@@ -0,0 +1,174 @@
+
+
+export const API = [
+  {
+    name: 'Stephen',
+    lastname: 'Curry',
+    team: 'Golden State Warriors',
+    salary: 48070014,
+    image: 'https://library.sportingnews.com/styles/twitter_card_120x120/s3/2022-06/sporting-news-2022-photo-with-watermark-cb005c60-e9cd-4ab5-89b3-731cf999850d.png?itok=9LUNeP-c',
+  },
+  {
+    name: 'John',
+    lastname: 'Wall',
+    team: 'Los Angeles Clippers',
+    salary: 47345760,
+    image: 'https://pbs.twimg.com/media/FWTcwZmWAAMWYhh?format=jpg&name=900x900'
+  },
+  {
+    name: 'Russell',
+    lastname: 'Westbrook',
+    team: 'Los Angeles Lakers',
+    salary: 47063478,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Russell_Westbrook_%28March_21%2C_2022%29_%28cropped%29.jpg/716px-Russell_Westbrook_%28March_21%2C_2022%29_%28cropped%29.jpg'
+  },
+  {
+    name: 'LeBron',
+    lastname: 'James',
+    team: 'Los Angeles Lakers',
+    salary: 44474988,
+    image: 'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/06/02/16541781833904.jpg'
+  },
+  {
+    name: 'Kevin',
+    lastname: 'Durant',
+    team: 'Brooklyn Nets',
+    salary: 44119845,
+    image: 'https://library.sportingnews.com/styles/twitter_card_120x120/s3/2022-06/Kevin-Durant-063022-GETTY-FTR.jpg?itok=fw7gczBy'
+  },
+  {
+    name: 'Bradley',
+    lastname: 'Beal',
+    team: 'Washington Wizards',
+    salary: 43279250,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-02/Bradley%20Beal%20Washington%20Wizards.jpeg?itok=pkzHdxlN'
+  },
+  {
+    name: 'Paul',
+    lastname: 'George',
+    team: 'Los Angeles Clippers',
+    salary: 42492568,
+    image: 'https://library.sportingnews.com/styles/twitter_card_120x120/s3/2021-11/la-clippers-star-paul-george-is-quietly-putting-together-a-strong-mvp-case_1oaxdwldkpzim1m65q1pr3u7ht.png?itok=WGnVczuB'
+  },
+  {
+    name: 'Kawhi',
+    lastname: 'Leonard',
+    team: 'Los Angeles Clippers',
+    salary: 42492492,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2021-11/kawhi-leonard_2vuxi0bjxe6w1p0uy9b6ewlkf.jpg?itok=QKSM50-_'
+  },
+  {
+    name: 'Giannis',
+    lastname: 'Antetokounmpo',
+    team: 'Milwaukee Bucks',
+    salary: 42492492	,
+    image: 'https://cnnespanol.cnn.com/wp-content/uploads/2020/12/CNN-Giannis-Antetokounmpo.jpg?quality=100&strip=info'
+  },
+  {
+    name: 'Damian',
+    lastname: 'Lillard',
+    team: 'Portland Trail Blazers',
+    salary: 42492492,
+    image: 'https://deportres.com/wp-content/uploads/2022/07/12-7.jpg'
+  },
+  {
+    name: 'Klay',
+    lastname: 'Thompson',
+    team: 'Golden State Warriors',
+    salary: 40600080,
+    image: 'https://bolavip.com/__export/1639771029415/sites/bolavip/img/2021/12/17/nba_klay_thompson_warriors.jpg_1159711837.jpg'
+  },
+  {
+    name: 'Rudy',
+    lastname: 'Gobert',
+    team: 'Minnesota Timberwolves',
+    salary: 38172414,
+    image: 'https://www.basquetplus.com/sites/default/files/main/articles/1241745628.0.png'
+  },
+  {
+    name: 'Khris',
+    lastname: 'Middleton',
+    team: 'Milwaukee Bucks',
+    salary: 37984276,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-04/sporting-news-2022-photo-with-watermark-5a3a7c4f-1daf-4213-9392-f98409fbe1f5.png?itok=W_NuBCY1'
+  },
+  {
+    name: 'Anthony',
+    lastname: 'Davis',
+    team: 'Los Angeles Lakers',
+    salary: 37980720,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-01/anthony-davis_1573w4krdxp6e1qwgpxefjuxy0.jpg?itok=QpLOBYZh'
+  },
+  {
+    name: 'Jimmy',
+    lastname: 'Butler',
+    team: 'Miami Heat',
+    salary: 37653300,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-05/sporting-news-2022-photo-with-watermark-cccd48a3-850c-47ed-a3e0-038e4e606173.png?itok=5i6u4iOg'
+  },
+  {
+    name: 'Tobias',
+    lastname: 'Harris',
+    team: 'Philadelphia 76ers',
+    salary: 37633050,
+    image: 'https://i0.wp.com/thelibertyline.com/wp-content/uploads/2022/01/Tobias-Harris-Trade-Piece-1-26-22.jpg'
+  },
+  {
+    name: 'Trae',
+    lastname: 'Young',
+    team: 'Atlanta Hawks',
+    salary: 37096500,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-01/trae-young_1kcyzhmq68rqg1dp9kxt8s7r0u.jpg?itok=0yrvyA9H'
+  },
+  {
+    name: 'Zach',
+    lastname: 'LaVine',
+    team: 'Chicago Bulls',
+    salary: 37096500,
+    image: 'https://library.sportingnews.com/styles/crop_style_16_9_mobile_2x/s3/2022-04/Zach%20LaVine.png?itok=ibGl4B40'
+  },
+  {
+    name: 'Luka',
+    lastname: 'Doncic',
+    team: 'Dallas Mavericks',
+    salary: 37096500,
+    image: 'https://as01.epimg.net/baloncesto/imagenes/2022/02/18/nba/1645166016_510961_1645166122_noticia_normal_recorte1.jpg'
+  },
+  {
+    name: 'Kyrie',
+    lastname: 'Irving',
+    team: 'Brooklyn Nets',
+    salary: 36934550,
+    image: 'https://phantom-marca.unidadeditorial.es/ba2236eb2bbd47fd8d481939e92665c9/resize/1320/f/jpg/assets/multimedia/imagenes/2022/06/20/16557323528160.jpg'
+  },
+  {
+    name: 'Kemba',
+    lastname: 'Walker',
+    team: 'Detroit Pistons',
+    salary: 36596549,
+    image: 'https://basquetplus.com/sites/default/files/main/articles/KembaWalker.jpg'
+  },
+  {
+    name: 'Ben',
+    lastname: 'Simmons',
+    team: 'Brooklyn Nets',
+    salary: 35448672,
+    image: 'https://meridiano.net/fotos_meridiano/16489978721235144121.jpg'
+  },
+  {
+    name: 'Pascal',
+    lastname: 'Siakam',
+    team: 'Toronto Raptors',
+    salary: 35448672,
+    image: 'https://a.espncdn.com/photo/2021/0227/r820354_1296x518_5-2.jpg'
+  },
+  {
+    name: 'Karl-Anthony',
+    lastname: 'Towns',
+    team: 'Minnesota Timberwolves',
+    salary: 33833400,
+    image: 'https://www.basquetplus.com/sites/default/files/main/articles/towns.png'
+  },
+
+ 
+];
---
+++ styles/theme.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const GlobalStyle = styled.div`
+        width: '100%';
+        height: '100%';
+        display: inline-block;
+        background-color: green;
+        font-family: 'Ubuntu Bold';
+
+
+        @font-face {
+                font-family: "Ubuntu Bold";
+                src: url("./fonts/Ubuntu-Bold.ttf");
+              }
+`
